eb39db65fd69600ad4a3d76689794c98fc5ffc51
Luddifee <ludwig@ludwig-lehnert.de>
2021-08-05 20:25:59 +0200

    first real update

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
+BASIC_DESKTOP_PKGS = "gdm\nxfce4"
+
 all:
-	mkarchiso -v -w work_dir -o vatis_out vatis
+	mkarchiso -v -w /tmp/vatis_work_dir -o vatis_out vatis
+
+xfce4:
+	cp -rf vatis vatis_$@
+	mkdir -p vatis_$@/airootfs/etc/skel/.config
+	cp -r configs/$@ vatis_$@/airootfs/etc/skel/.config/
+	printf "\n#specific packages\n$@\n" >> vatis_$@/packages.x86_64
+	mkarchiso -v -w /tmp/vatis_$@_work_dir -o vatis_out vatis_$@
+
+run:
+	run_archiso -i vatis_out/$(shell ls -t vatis_out | head -n1)
 
 clean:
-	rm -rf work_dir
+	rm -rf /tmp/vatis_*_work_dir
--- vatis/profiledef.sh
+++ vatis/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="vatislinux"
+iso_label="VATIS_$(date +%Y%m)"
+iso_publisher="Vatis Linux <https://github.com/Luddifee/vatis-linux>"
+iso_application="Vatis Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -15,6 +15,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:0400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
